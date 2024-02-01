4d711e57dd9f2f3194baeeaa545fd154dd63a77a
Sameerkhan7509 <sameer.khan@accelgrowthtech.com>
2024-02-01 15:12:14 +0530

    error-fix

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import "./style.css";
+
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="pageNotFound h-[700px] pt-[200px]">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default PageNotFound;
